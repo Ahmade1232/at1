abbb87ff20e4eb73ec62f77fe7ab4b2b58c98a57
Ahmad <peacefull1575@gmail.com>
2024-03-27 14:08:56 +1100

    random questions

--- eduprod/views.py
+++ eduprod/views.py
@@ -1,3 +1,12 @@
+from django.core import serializers
 from django.shortcuts import render
+from .models import Question
+import random
 
-# Create your views here.
+def index(request):
+    questions = Question.objects.all()
+    questions_list = list(questions)  # Convert queryset to list
+    random.shuffle(questions_list)  # Shuffle the list
+    questions_json = serializers.serialize('json', questions_list[:2])
+    #questions_json = serializers.serialize('json', questions_list)
+    return render(request, 'eduprod/index.html', {'questions_json': questions_json})
